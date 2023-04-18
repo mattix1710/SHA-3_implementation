afdfe8a48fc819b5a5a76be641bf79f25caa7777
Mateusz Szuda <mateusz.szuda@wp.pl>
2023-04-18 22:59:15 +0200

    Update keccak_func.py
    Added theta function

--- keccak_func.py
+++ keccak_func.py
@@ -8,25 +8,50 @@ import numpy as np
 import os
 from pathlib import Path
 
+KECCAK_BYTES = 200
+KECCAK_LANES = 25
+KECCAK_PLANES_SLICES = 5
+
 def Keccak_subfuncs(state):
     '''
     uint64 which gives us 64 bits per 1 "width" which *25 = 1600 bit state array
     
     F-order (which is also "Fortran order") implements 2D array as [X][Y] positions
+    C-order implements 2D array as [Y][X] array of positions
     '''
     # orig code
     # state = np.copy(np.frombuffer(state, dtype=np.uint64, count=25))#.reshape([5, 5], order='F'))
     
-    state_arr = np.ndarray((5,5), dtype=np.uint64, buffer=state, order='F')
+    state_arr = np.ndarray((5,5), dtype=np.uint64, buffer=state, order='C')
 
     # === Printing data ===
-    print(state_arr)
-    # print(state_arr[0][0])
-    bytes_val = state_arr[1][1].tobytes()
-    print(bytes_val)
+    # print(state_arr)
+    # # print(state_arr[0][0])
+    # bytes_val = state_arr[0][0].tobytes()
+    # print(bytes_val)
     
     # === Theta function ===
+    # even out left and right columns
+    leftShift = state_arr << 1 | state_arr >> 63
+    rightShift = state_arr >> 1 | state_arr << 63
     
+    # flatten columns - because each bit uses whole y-dimension
+    leftFlat = np.zeros((5), dtype=np.uint64)
+    rightFlat = np.zeros((5), dtype=np.uint64)
+    
+    for y in range(KECCAK_PLANES_SLICES):
+        leftFlat ^= leftShift[y]
+        rightFlat ^= rightShift[y]
+        
+    # XOR columns depending on their position
+    columnsXORed = np.zeros_like(leftFlat)
+    for x in range(KECCAK_PLANES_SLICES):
+        columnsXORed[x] = leftFlat[(x-1)%5] ^ rightFlat[(x+1)%5]
+    
+    # XOR each bit with calculated columns
+    for x in range(KECCAK_PLANES_SLICES):
+        for y in range(KECCAK_PLANES_SLICES):
+            state_arr[y][x] ^= columnsXORed[x]
     
 
 def Keccak_256(inputBytes):
